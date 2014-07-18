7e0fc992d23aacbf0294159de040cdee8aef1f84
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2014-07-18 20:19:24 +0000

    Added chameleon
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@130 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,9 @@
 export PATH=${PATH}:/home/markw/fpga/altera/13.0sp1/quartus/bin
+#export PATH=${PATH}:/home/markw/fpga/altera/14.0/quartus/bin
 cd de1
 ./build.sh > build.log 2> build.err&
+cd chameleon
+./build.sh ALL > build.log 2> build.err&
 cd ../replay
 ./build.sh > build.log 2> build.err&
 cd ../mist
---
+++ package.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+use POSIX qw(strftime);
+
+my @files;
+
+push @files, glob("mcc216/build*/output_files/*.arg");
+push @files, glob("mcc216/build*/output_files/*.sof");
+push @files, glob("mcc216/build*/output_files/*.rbf");
+push @files, glob("mcc216/build*/output_files/*.summary");
+push @files, glob("mcc216/build*/output_files/*.rpt");
+
+push @files, glob("mist/build*/out/*.sof");
+push @files, glob("mist/build*/out/*.rbf");
+push @files, glob("mist/build*/out/*.summary");
+push @files, glob("mist/build*/out/*.rpt");
+
+push @files, glob("chameleon/build*/out/*.sof");
+push @files, glob("chameleon/build*/out/*.rbf");
+push @files, glob("chameleon/build*/out/*.summary");
+push @files, glob("chameleon/build*/out/*.rpt");
+
+push @files, glob("de1/build*/output_files/*.sof");
+push @files, glob("de1/build*/output_files/*.summary");
+push @files, glob("de1/build*/output_files/*.rpt");
+
+push @files, glob("replay/sdcard/*.bin");
+push @files, glob("replay/sdcard/*.ini");
+
+mkdir "/var/www/html/autobuild/";
+my $date = strftime("%Y%m%d",gmtime);
+my $dir = "/var/www/html/autobuild/$date";
+mkdir $dir;
+open (LOG,">".$dir."/log") or die "Failed to open log";
+foreach (@files)
+{
+	my $creationtime = (stat($_))[9];
+	my $creation = strftime("%Y%m%dT%T",gmtime($creationtime));
+	print LOG "File:$_ Date:$creation\n";
+
+	/(.*)\/(.*)/;
+	my ($dir2,$file) = ($1,$2);
+	#print "DIR:$dir2 FILE:$file\n";
+	`mkdir -p $dir/$dir2`;
+
+	`cp -f $_ $dir/$dir2`;
+}
+close(LOG);
+`cp -f instructions.txt $dir/`;
+
+
