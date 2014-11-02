0b9d22e7a6da72e984776f0de87030feb781c8ed
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2014-11-02 21:56:19 +0000

    Removed delay
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@254 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- firmware/5200/joystick.c
+++ firmware/5200/joystick.c
@@ -19,13 +19,13 @@ void joystick_poll(struct joystick_status * status)
 		kbcode = 0x0;
 	}
 
-	*atari_consol = 4;
+	/**atari_consol = 4;
 	*atari_potgo = 0xff;
 
 	wait_us(1000000/50);
 
 	unsigned char pot0 = *atari_pot0;
-	unsigned char pot1 = *atari_pot1;
+	unsigned char pot1 = *atari_pot1;*/
 
 	status->y_ = (0x8==(kbcode&0x18)) -((unsigned int)(0x18==(kbcode&0x18)));
 	status->x_ = (0x2==(kbcode&0x6)) -((unsigned int)(0x6==(kbcode&0x6)));
