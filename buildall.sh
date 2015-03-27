d1f289558381eba1193c3639e631bf1d87289ede
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2015-03-27 20:19:54 +0000

    Do not background - not got enough ram here
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@343 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- buildall.sh
+++ buildall.sh
@@ -6,23 +6,23 @@ cd de1
 cd ../chameleon
 ./build.sh ALL
 cd ../replay
-./build.sh > build.log 2> build.err&
+./build.sh > build.log 2> build.err
 cd ../mist
 ./build.sh ALL
 cd ../mcc216
-./build.sh ALL &
+./build.sh ALL
 cd ../mcctv
-./build.sh ALL &
+./build.sh ALL
 cd ../papilioduo
-./build.sh ALL &
+./build.sh ALL
 cd ../mist_5200
 ./build.sh ALL
 cd ../de1_5200
 ./build.sh ALL
 cd ../mcc216_5200
-./build.sh ALL &
+./build.sh ALL
 cd ../mcctv_5200
-./build.sh ALL &
+./build.sh ALL
 
 
 export PATH=${ORIGPATH}:/home/markw/fpga/altera/14.0/quartus/bin:/home/markw/fpga/altera/14.0/quartus/sopc_builder/bin/
