b28faa646263e6a28d9715922767e9a9074bfede
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2015-08-07 06:39:00 +0000

    Added return to boot menu to a800 mcc cores
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@389 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- mcctv/build.sh
+++ mcctv/build.sh
@@ -52,6 +52,8 @@ foreach my $variant (sort keys %variants)
 	mkdir $dir;
 	`cp atari800core_mcc.vhd $dir`;
 	`cp *pll*.* $dir`;
+	`cp ../mcc_common/*remote_update*.* $dir`;
+	`cp ../mcc_common/*delayed_reconfig*.* $dir`;
 	`cp sdram_ctrl_3_ports.v $dir`;
 	`cp zpu_rom.vhdl $dir`;
 	`cp atari800core.sdc $dir`;
