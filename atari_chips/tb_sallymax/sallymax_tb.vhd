cb65f721c4796b5c2f07d32044aed9f104fdde71
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2018-10-22 17:53:56 +0000

    Some uncommited sallymax testbench changes
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@808 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- atari_chips/tb_sallymax/sallymax_tb.vhd
+++ atari_chips/tb_sallymax/sallymax_tb.vhd
@@ -12,16 +12,30 @@ end;
 
 architecture rtl of sallymax_tb is
 
-  constant CLK_BUS_PERIOD : time := 1 us / (1.79*32);
+  constant CLK_BUS_PERIOD : time := 1 us / (1.79);
+  constant CLK_FAST_PERIOD : time := 1 us / (1.79*32);
 
   signal reset_n : std_logic;
   signal clk_phi0 : std_logic;
+  signal clk_fast : std_logic;
 
   signal clk_routed : std_logic;
 
   signal bus_addr : std_logic_vector(15 downto 0);
   signal bus_data : std_logic_vector(7 downto 0);
+  signal bus_data_out : std_logic_vector(7 downto 0);
+  signal bus_drive : std_logic;
   signal bus_rw : std_logic;
+
+  signal bus_phi1 : std_logic;
+  signal bus_phi2 : std_logic;
+
+  signal slave_request : std_logic;
+  signal slave_addr : std_logic_vector(15 downto 0);
+  signal slave_data_in : std_logic_vector(7 downto 0);
+  signal slave_data_out : std_logic_vector(7 downto 0);
+  signal slave_rw_n : std_logic;
+  signal slave_cs : std_logic;
 
 begin
 	p_clk_gen_b : process
@@ -30,6 +44,14 @@ begin
 	wait for CLK_BUS_PERIOD/2;
 	clk_phi0 <= '0';
 	wait for CLK_BUS_PERIOD - (CLK_BUS_PERIOD/2 );
+	end process;
+
+	p_clk_gen_f : process
+	begin
+	clk_fast <= '1';
+	wait for CLK_FAST_PERIOD/2;
+	clk_fast <= '0';
+	wait for CLK_FAST_PERIOD - (CLK_FAST_PERIOD/2 );
 	end process;
 
 	reset_n <= '0', '1' after 1000ns;
@@ -51,8 +73,83 @@ begin
 		HALT_N => '1',
 		NMI_N => '1',
 		IRQ_N => '1',
-		S0 => '1'
+		S0 => '1',
+
+		phi2 => bus_phi2,
+		phi1 => bus_phi1
 	);
+
+	busadapt : entity work.slave_timing_6502
+	port map
+	(
+		CLK => clk_fast,
+		RESET_N => reset_n,
+				
+		PHI2 => bus_phi2,
+		bus_addr => bus_addr,
+		bus_data => bus_data,
+		bus_cs => '1',
+		bus_rw_n => bus_rw,
+
+		bus_data_out => bus_data_out,
+		bus_drive => bus_drive,
+
+		BUS_REQUEST => slave_request,
+		ADDR_IN => slave_addr, 
+		DATA_IN => slave_data_in,
+		RW_N => slave_rw_n,
+		CS => slave_cs,
+
+		ENABLE_CYCLE => open,
+
+		DATA_OUT => slave_data_out
+	);
+
+	bus_data <= bus_data_out when bus_drive='1' else (others=>'Z');
+
+	process
+	begin
+		wait until reset_n='1';
+
+		wait until slave_request='1';
+		slave_data_out <= x"00";
+		wait until slave_request='0';
+
+		wait until slave_request='1';
+		slave_data_out <= x"e0";
+		wait until slave_request='0';
+
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+
+		wait until slave_request='1';
+		slave_data_out <= x"ea";
+		wait until slave_request='0';
+		
+	end process;
 
 end rtl;
 
---
+++ atari_chips/tb_sallymax/slave_timing_6502.vhd
@@ -0,0 +1,189 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+use IEEE.STD_LOGIC_MISC.all;
+
+ENTITY slave_timing_6502 IS
+	PORT (
+				CLK: in std_logic;
+				RESET_N: in std_logic;
+				
+				-- input from the cart port
+				PHI2 : in std_logic; -- async to our clk (ish):-(
+				bus_addr : in std_logic_vector(15 downto 0);
+				bus_data : in std_logic_vector(7 downto 0);
+				bus_cs : in std_logic;
+				bus_rw_n : in std_logic;
+	
+				-- output to the cart port
+				bus_data_out : out std_logic_vector(7 downto 0);
+				bus_drive : out std_logic;
+
+				-- request for a memory bus cycle (read or write)
+				BUS_REQUEST: out std_logic;
+				ADDR_IN: out std_logic_vector(15 downto 0);
+				DATA_IN: out std_logic_vector(7 downto 0);
+				RW_N: out std_logic;
+				CS : out std_logic;
+
+				ENABLE_CYCLE : out std_logic;
+
+				DATA_OUT: in std_logic_vector(7 downto 0) -- read_data
+			);
+END slave_timing_6502;
+
+ARCHITECTURE vhdl OF slave_timing_6502 IS
+
+	signal PHI2_sync : std_logic;
+	
+	signal phi_edge_prev_next : std_logic;
+	signal phi_edge_prev_reg: std_logic;	
+
+	signal delay_next : std_logic_vector(31 downto 0);	
+	signal delay_reg : std_logic_vector(31 downto 0);
+	
+	signal bus_data_out_next : std_logic_vector(7 downto 0);	
+	signal bus_data_out_reg : std_logic_vector(7 downto 0);
+
+	signal bus_drive_next : std_logic;
+	signal bus_drive_reg : std_logic;
+
+	signal bus_data_in_next : std_logic_vector(7 downto 0);	
+	signal bus_data_in_reg : std_logic_vector(7 downto 0);
+	signal bus_addr_in_next : std_logic_vector(15 downto 0);	
+	signal bus_addr_in_reg : std_logic_vector(15 downto 0);
+	signal bus_rw_n_next : std_logic;
+	signal bus_rw_n_reg : std_logic;
+	signal bus_cs_next : std_logic;
+	signal bus_cs_reg : std_logic;
+
+	signal state_reg : std_logic_vector(2 downto 0);
+	signal state_next : std_logic_vector(2 downto 0);
+	constant state_wait_addrctl : std_logic_vector(2 downto 0) := "001";
+	constant state_write_request : std_logic_vector(2 downto 0) := "010";
+	constant state_read_output_start : std_logic_vector(2 downto 0) := "011";
+	constant state_read_output_end : std_logic_vector(2 downto 0) := "100";
+	constant state_read_output_fetch : std_logic_vector(2 downto 0) := "101";
+
+	signal internal_memory_request : std_logic;
+	signal registered_read_data_next : std_logic_vector(7 downto 0);
+	signal registered_read_data_reg : std_logic_vector(7 downto 0);
+begin
+	-- Fast half, for accurate sampling of the 6502 bus - which is quirky on Atari - e.g. phi2 is often not in time with the data lines on writes!!
+
+	process(clk,reset_n)
+	begin
+		if (reset_n='0') then
+			phi_edge_prev_reg <= '1';
+			delay_reg <= (others=>'0');
+			bus_data_out_reg <= (others=>'0');
+			bus_drive_reg <= '0';
+
+			bus_cs_reg <= '1';
+			bus_rw_n_reg <= '1';
+			bus_data_in_reg <= (others=>'0');
+			bus_addr_in_reg <= (others=>'0');
+
+			registered_read_data_reg <= (others=>'0');
+
+			state_reg <= state_wait_addrctl;
+		elsif (clk'event and clk='1') then
+			phi_edge_prev_reg <= phi_edge_prev_next;
+			delay_reg <= delay_next;
+			bus_data_out_reg <= bus_data_out_next;
+			bus_drive_reg <= bus_drive_next;
+
+			registered_read_data_reg <= registered_read_data_next;
+
+			bus_cs_reg <= bus_cs_next;
+			bus_rw_n_reg <= bus_rw_n_next;
+			bus_data_in_reg <= bus_data_in_next;
+			bus_addr_in_reg <= bus_addr_in_next;
+
+			state_reg <= state_next;
+		end if;
+	end process;
+	
+	synchronizer_phi : entity work.synchronizer
+				 port map (clk=>clk, raw=>PHI2, sync=>PHI2_SYNC);
+
+	phi_edge_prev_next <= phi2_sync;
+
+	process(registered_read_data_reg, data_out,phi2_sync, phi_edge_prev_reg, delay_reg, 
+		bus_drive_reg,bus_data_out_reg, 
+		bus_rw_n_reg,bus_addr_in_reg,bus_data_in_reg,
+		bus_rw_n,
+		bus_cs,bus_cs_reg,
+		bus_data,bus_addr,
+		state_reg)
+	begin
+		-- maintain snap (only read bus when safe!)
+		bus_addr_in_next <= bus_addr_in_reg;
+		bus_data_in_next <= bus_data_in_reg;
+		bus_rw_n_next <= bus_rw_n_reg;
+
+		internal_memory_request <= '0';
+		delay_next <= delay_reg(30 downto 0)&(not(phi2_sync) and phi_edge_prev_reg);
+		bus_data_out_next <= bus_data_out_reg;
+		bus_drive_next <= bus_drive_reg;
+		bus_cs_next <= bus_cs_reg;
+
+		registered_read_data_next <= registered_read_data_reg;
+
+		-- LLLLLLLHHHHHHH
+		-- XXAAAAAAAAAAAA
+		-- XXXXXXXXXXDDDD
+		state_next <= state_reg;
+		case (state_reg) is
+			when state_wait_addrctl =>
+				if ((bus_cs and delay_reg(11))='1') then
+					-- snap control signals, should be stable by now
+					bus_addr_in_next <= bus_addr;
+					bus_rw_n_next <= bus_rw_n;
+					bus_cs_next <= bus_cs;
+
+					if (bus_rw_n='1') then -- read
+						state_next <= state_read_output_fetch;
+					else
+						state_next <= state_write_request;
+					end if;
+				end if;
+			when state_write_request =>
+				if (delay_reg(26)='1') then
+					bus_data_in_next <= bus_data;
+				end if;
+				if (delay_reg(27)='1') then
+					internal_memory_request <= '1';
+					state_next <= state_wait_addrctl;
+				end if;
+			when state_read_output_fetch =>
+				state_next <= state_read_output_start;
+				internal_memory_request <= '1';
+				registered_read_data_next <= data_out;
+			when state_read_output_start =>
+				if (delay_reg(20)='1') then
+					bus_data_out_next <= registered_read_data_reg;
+					bus_drive_next <= '1';
+					state_next <= state_read_output_end;
+				end if;
+			when state_read_output_end =>
+				if (delay_reg(31)='1') then
+					bus_drive_next <= '0';
+					state_next <= state_wait_addrctl;
+				end if;
+			when others =>
+		end case;
+		
+	end process;
+
+	bus_data_out <= bus_data_out_reg;
+	bus_drive <= bus_drive_reg;
+	bus_request <= internal_memory_request;
+	addr_in <= bus_addr_in_reg;
+	data_in <= bus_data_in_reg;
+	rw_n <= bus_rw_n_reg;	
+	CS <= bus_cs_reg;
+
+	enable_cycle <= delay_reg(29);
+
+end vhdl;
