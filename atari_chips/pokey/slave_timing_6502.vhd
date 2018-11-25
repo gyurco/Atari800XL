c95fc4b1a4560d8525d00647ae13ae5665308f42
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2018-11-25 21:52:27 +0000

    Use the phi2 clock instead of the delay based timing
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@831 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- atari_chips/pokey/slave_timing_6502.vhd
+++ atari_chips/pokey/slave_timing_6502.vhd
@@ -39,96 +39,143 @@ ARCHITECTURE vhdl OF slave_timing_6502 IS
 	signal phi_edge_prev_next : std_logic;
 	signal phi_edge_prev_reg: std_logic;	
 
-	signal delay_next : std_logic_vector(31 downto 0);	
-	signal delay_reg : std_logic_vector(31 downto 0);
-	
 	signal bus_data_out_next : std_logic_vector(7 downto 0);	
 	signal bus_data_out_reg : std_logic_vector(7 downto 0);
 
 	signal bus_drive_next : std_logic;
 	signal bus_drive_reg : std_logic;
 
-	signal bus_data_in_next : std_logic_vector(7 downto 0);	
-	signal bus_data_in_reg : std_logic_vector(7 downto 0);
-	signal bus_addr_in_next : std_logic_vector(4 downto 0);	
-	signal bus_addr_in_reg : std_logic_vector(4 downto 0);
-	signal bus_rw_n_next : std_logic;
-	signal bus_rw_n_reg : std_logic;
-	signal bus_cs_next : std_logic;
-	signal bus_cs_reg : std_logic;
-
-	signal state_reg : std_logic_vector(2 downto 0);
-	signal state_next : std_logic_vector(2 downto 0);
-	constant state_wait_addrctl : std_logic_vector(2 downto 0) := "001";
-	constant state_write_request : std_logic_vector(2 downto 0) := "010";
-	constant state_read_output_start : std_logic_vector(2 downto 0) := "011";
-	constant state_read_output_end : std_logic_vector(2 downto 0) := "100";
-	constant state_read_output_fetch : std_logic_vector(2 downto 0) := "101";
+	signal state_reg : std_logic_vector(1 downto 0);
+	signal state_next : std_logic_vector(1 downto 0);
+	constant state_wait_addrctl : std_logic_vector(1 downto 0) := "01";
+	constant state_write_request : std_logic_vector(1 downto 0) := "10";
+	constant state_read_output : std_logic_vector(1 downto 0) := "11";
 
 	signal internal_memory_request : std_logic;
 	signal registered_read_data_next : std_logic_vector(7 downto 0);
 	signal registered_read_data_reg : std_logic_vector(7 downto 0);
+
+	signal phi2_falling_edge : std_logic;
+	signal phi2_rising_edge : std_logic;
+	signal phi2_falling_edge_reg : std_logic;
+
+	signal phi_addr_reg : std_logic_vector(4 downto 0);
+	signal phi_cs_reg : std_logic;
+	signal phi_rw_n_reg : std_logic;
+	signal phi_data_reg : std_logic_vector(7 downto 0);
 begin
-	-- Fast half, for accurate sampling of the 6502 bus - which is quirky on Atari - e.g. phi2 is often not in time with the data lines on writes!!
+	-- phi2 half, phi2 is in sync properly within the atari, the out of sync stuff is a feature of the PBI buffer I believe
+	process(phi2,reset_n)
+	begin
+		if (reset_n='0') then
+			phi_data_reg <= (others=>'0');
+		elsif (phi2'event and phi2='0') then -- falling edge
+			phi_data_reg <= bus_data;
+		end if;
+	end process;
+
+	process(phi2,reset_n)
+	begin
+		if (reset_n='0') then
+			phi_addr_reg <= (others=>'0');
+			phi_cs_reg <= '1';
+			phi_rw_n_reg <= '1';
+		elsif (phi2'event and phi2='1') then -- rising edge
+			phi_addr_reg <= bus_addr;
+			phi_cs_reg <= bus_cs;
+			phi_rw_n_reg <= bus_rw_n;
+		end if;
+	end process;
+
+	-- Fast half
 
 	process(clk,reset_n)
 	begin
 		if (reset_n='0') then
 			phi_edge_prev_reg <= '1';
-			delay_reg <= (others=>'0');
 			bus_data_out_reg <= (others=>'0');
 			bus_drive_reg <= '0';
 
-			bus_cs_reg <= '1';
-			bus_rw_n_reg <= '1';
-			bus_data_in_reg <= (others=>'0');
-			bus_addr_in_reg <= (others=>'0');
-
 			registered_read_data_reg <= (others=>'0');
+
+			phi2_falling_edge_reg <= '0';
 
 			state_reg <= state_wait_addrctl;
 		elsif (clk'event and clk='1') then
 			phi_edge_prev_reg <= phi_edge_prev_next;
-			delay_reg <= delay_next;
 			bus_data_out_reg <= bus_data_out_next;
 			bus_drive_reg <= bus_drive_next;
 
 			registered_read_data_reg <= registered_read_data_next;
 
-			bus_cs_reg <= bus_cs_next;
-			bus_rw_n_reg <= bus_rw_n_next;
-			bus_data_in_reg <= bus_data_in_next;
-			bus_addr_in_reg <= bus_addr_in_next;
+			phi2_falling_edge_reg <= phi2_falling_edge;
 
 			state_reg <= state_next;
 		end if;
 	end process;
 	
-	synchronizer_phi : entity work.synchronizer
-				 port map (clk=>clk, raw=>PHI2, sync=>PHI2_SYNC);
+	synchronizer_phi : entity work.synchronizer
+				 port map (clk=>clk, raw=>PHI2, sync=>PHI2_SYNC);
 
 	phi_edge_prev_next <= phi2_sync;
 
-	process(registered_read_data_reg, data_out,phi2_sync, phi_edge_prev_reg, delay_reg, 
+	phi2_falling_edge <= phi_edge_prev_reg and not(phi2_sync);
+	phi2_rising_edge <= not(phi_edge_prev_reg) and phi2_sync;
+
+	process(registered_read_data_reg, data_out,
 		bus_drive_reg,bus_data_out_reg, 
-		bus_rw_n_reg,bus_addr_in_reg,bus_data_in_reg,
-		bus_rw_n,
-		bus_cs,bus_cs_reg,
-		bus_data,bus_addr,
+		phi2_rising_edge,
+		phi2_falling_edge,
 		state_reg)
 	begin
 		-- maintain snap (only read bus when safe!)
-		bus_addr_in_next <= bus_addr_in_reg;
-		bus_data_in_next <= bus_data_in_reg;
-		bus_rw_n_next <= bus_rw_n_reg;
-
 		internal_memory_request <= '0';
-		delay_next <= delay_reg(30 downto 0)&(not(phi2_sync) and phi_edge_prev_reg);
 		bus_data_out_next <= bus_data_out_reg;
 		bus_drive_next <= bus_drive_reg;
-		bus_cs_next <= bus_cs_reg;
 
 		registered_read_data_next <= registered_read_data_reg;
+
+		-- TODO: Drive off rise/fall of phi2
+		-- delays should be based off tsu/th times from data shet
+		-- I heard the data from cpu is not always stable on writes at the clock edge, might need to have a short buffer and wait for edge then read etc.
+
+		-- IN:
+		-- RW/ADDR tSU (130ns), tH (30ns)
+		-- CS tSU (50ns), tH (30ns)
+		-- D tSU (130ns), tH (10ns)
+		-- OUT:
+		-- D tSU (50ns), tH (20ns)
+		-- IRQ tDELAY (350ns??) 
+
+		-- write timing
+		-- r/w read on rising edge of phi2  -> HELD until falling edge of phi2 + tH
+		-- CS read on rising edge of phi2   -> HELD until falling edge of phi2 + tH
+		-- addr read on rising edge of phi2 -> HELD until phi2 is risen
+
+		-- read timing
+		-- r/w similar
+		-- cs similar
+		-- drive data tDSR before falling edge - I guess it makes sense to drive it as early after phi2 risen as possible
+
+		-- concern: hold times are known not to be respected, best to read prior to phi2 by some ns
+		-- issue with that: we don't know the clock
+		-- solution: trigger read/write on phi2 edge, but... use data from a cycle before -> 1 cycle 17ns. 
+
+		-- current solution -> 	
+		-- phi2 edge + 13 cycles -> snap addr,rw_n,cs_n -> 232ns
+		-- read: then runs pokey, then output from 22->33 cycles -> 392 to 590ns
+		-- write: snap bus at cycle 28 -> 500ns
+		-- pokey run at 553ns
+		-- which is fine for in Atari 8-bit, but not arcade...
+
+		-- since cycle length:
+		-- 1.00MHz -> 1000ns
+		-- 1.25MHz ->  800ns
+		-- 1.50MHz ->  666ns
+		-- 1.77MHz ->  564ns
+		-- 1.79MHz ->  558ns
+
+		-- solution is to measure
 
 		-- LLLLLLLHHHHHHH
 		-- XXAAAAAAAAAAAA
@@ -136,39 +183,25 @@ begin
 		state_next <= state_reg;
 		case (state_reg) is
 			when state_wait_addrctl =>
-				if ((bus_cs and delay_reg(11))='1') then
+				if (phi2_rising_edge='1' and phi_cs_reg='1') then
 					-- snap control signals, should be stable by now
-					bus_addr_in_next <= bus_addr;
-					bus_rw_n_next <= bus_rw_n;
-					bus_cs_next <= bus_cs;
-
-					if (bus_rw_n='1') then -- read
-						state_next <= state_read_output_fetch;
+					if (phi_rw_n_reg='1') then -- read
+						internal_memory_request <= '1';
+						registered_read_data_next <= data_out;
+						state_next <= state_read_output;
 					else
 						state_next <= state_write_request;
 					end if;
 				end if;
 			when state_write_request =>
-				if (delay_reg(26)='1') then
-					bus_data_in_next <= bus_data;
-				end if;
-				if (delay_reg(27)='1') then
+				if (phi2_falling_edge='1') then
 					internal_memory_request <= '1';
 					state_next <= state_wait_addrctl;
 				end if;
-			when state_read_output_fetch =>
-				state_next <= state_read_output_start;
-				internal_memory_request <= '1';
-				registered_read_data_next <= data_out;
-			when state_read_output_start =>
-				if (delay_reg(20)='1') then
-					bus_data_out_next <= registered_read_data_reg;
-					bus_drive_next <= '1';
-					state_next <= state_read_output_end;
-				end if;
-			when state_read_output_end =>
-				if (delay_reg(31)='1') then
-					bus_drive_next <= '0';
+			when state_read_output =>
+				bus_data_out_next <= registered_read_data_reg;
+				bus_drive_next <= not(phi2_falling_edge);
+				if (phi2_falling_edge='1') then
 					state_next <= state_wait_addrctl;
 				end if;
 			when others =>
@@ -179,11 +212,11 @@ begin
 	bus_data_out <= bus_data_out_reg;
 	bus_drive <= bus_drive_reg;
 	bus_request <= internal_memory_request;
-	addr_in <= bus_addr_in_reg;
-	data_in <= bus_data_in_reg;
-	rw_n <= bus_rw_n_reg;	
-	CS <= bus_cs_reg;
+	addr_in <= phi_addr_reg;
+	data_in <= phi_data_reg;
+	rw_n <= phi_rw_n_reg;	
+	CS <= phi_cs_reg;
 
-	enable_cycle <= delay_reg(29);
+	enable_cycle <= phi2_falling_edge_reg;
 
 end vhdl;
