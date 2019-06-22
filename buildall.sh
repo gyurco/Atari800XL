0780d9d6679169a962a44a1e47d73fe379de2fc5
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2019-06-22 20:08:35 +0000

    Getting ready to release some core boards
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@908 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- buildall.sh
+++ buildall.sh
@@ -10,35 +10,39 @@ function max3 {
 }
 
 export PATH=${ORIGPATH}:/home/markw/fpga/altera/13.0sp1/quartus/bin
-cd de1
-max3; ./build.sh ALL &
+#cd de1
+#max3; ./build.sh ALL &
 cd ../chameleon
 max3; ./build.sh ALL &
-cd ../replay
-max3; ./build.sh > build.log 2> build.err &
+#cd ../replay
+#max3; ./build.sh > build.log 2> build.err &
 cd ../mist
 max3; ./build.sh ALL &
 cd ../mcc216
 max3; ./build.sh ALL &
 cd ../mcctv
 max3; ./build.sh ALL &
-cd ../papilioduo
-max3; ./build.sh ALL &
-cd ../aeon_lite
-max3; ./build.sh ALL &
+#cd ../papilioduo
+#max3; ./build.sh ALL &
+#cd ../aeon_lite
+#max3; ./build.sh ALL &
 cd ../mist_5200
 max3; ./build.sh ALL &
-cd ../de1_5200
-max3; ./build.sh ALL &
+#cd ../de1_5200
+#max3; ./build.sh ALL &
 cd ../mcc216_5200
 max3; ./build.sh ALL &
 cd ../mcctv_5200
 max3; ./build.sh ALL &
 
 
-export PATH=${ORIGPATH}:/home/markw/fpga/altera/15.0/quartus/bin:/home/markw/fpga/altera/15.0/quartus/sopc_builder/bin/
-cd ../sockit
-max3; ./build.sh &
+export PATH=${ORIGPATH}:/home/markw/intelFPGA_lite/18.0/quartus/bin:/home/markw/intelFPGA_lite/18.0/quartus/sopc_builder/bin/
+#cd ../sockit
+#max3; ./build.sh &
+cd ../chameleon2
+max3; ./build.sh ALL &
+cd ../eclaireXL_ITX
+max3; ./build.sh ALL &
 
 export PATH=${ORIGPATH}
 unset ORIGPATH
