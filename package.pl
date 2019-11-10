b49de5d10165a0637a567d277eeffc53a1914821
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2019-11-10 12:46:53 +0000

    Package a few additional files
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@961 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- package.pl
+++ package.pl
@@ -8,6 +8,7 @@ push @files, glob("mcc216/build*/output_files/*.sof");
 push @files, glob("mcc216/build*/output_files/*.rbf");
 push @files, glob("mcc216/build*/output_files/*.summary");
 push @files, glob("mcc216/build*/output_files/*.rpt");
+push @files, glob("mcc216/sdflash.bin");
 
 push @files, glob("mcc216_5200/build*/output_files/*.arg");
 push @files, glob("mcc216_5200/build*/output_files/*.sof");
@@ -20,6 +21,7 @@ push @files, glob("mcctv/build*/output_files/*.sof");
 push @files, glob("mcctv/build*/output_files/*.rbf");
 push @files, glob("mcctv/build*/output_files/*.summary");
 push @files, glob("mcctv/build*/output_files/*.rpt");
+push @files, glob("mcctv/sdflash.bin");
 
 push @files, glob("mcctv_5200/build*/output_files/*.arg");
 push @files, glob("mcctv_5200/build*/output_files/*.sof");
@@ -31,6 +33,7 @@ push @files, glob("mist/build*/out/*.sof");
 push @files, glob("mist/build*/out/*.rbf");
 push @files, glob("mist/build*/out/*.summary");
 push @files, glob("mist/build*/out/*.rpt");
+push @files, glob("mist/sdflash.bin");
 
 push @files, glob("mist_5200/build*/out/*.sof");
 push @files, glob("mist_5200/build*/out/*.rbf");
@@ -41,17 +44,20 @@ push @files, glob("chameleon/build*/output_files/*.sof");
 push @files, glob("chameleon/build*/output_files/*.rbf");
 push @files, glob("chameleon/build*/output_files/*.summary");
 push @files, glob("chameleon/build*/output_files/*.rpt");
+push @files, glob("chameleon/chameleon.bin");
 
 push @files, glob("chameleon2/build*/output_files/*.sof");
 push @files, glob("chameleon2/build*/output_files/*.rbf");
 push @files, glob("chameleon2/build*/output_files/*.summary");
 push @files, glob("chameleon2/build*/output_files/*.rpt");
+push @files, glob("chameleon2/chameleon.bin");
 
 push @files, glob("eclaireXL_ITX/build*/output_files/*.sof");
 push @files, glob("eclaireXL_ITX/build*/output_files/*.rbf");
 push @files, glob("eclaireXL_ITX/build*/output_files/*.rbd");
 push @files, glob("eclaireXL_ITX/build*/output_files/*.summary");
-push @files, glob("eclaireXL_ITX/build*/output_files/*.rpt");
+push @files, glob("eclaireXL_ITX/build*/output_files/*.rpd");
+push @files, glob("eclaireXL_ITX/build*/output_files/*.jic");
 
 #push @files, glob("de1/build*/output_files/*.sof");
 #push @files, glob("de1/build*/output_files/*.pof");
@@ -79,7 +85,7 @@ push @files, glob("eclaireXL_ITX/build*/output_files/*.rpt");
 #push @files, glob("replay/sdcard/*.bin");
 #push @files, glob("replay/sdcard/*.ini");
 
-my $DEST="/home/markw/fpga/svn/repo/trunk/atari_800xl/packaged/"
+my $DEST="/home/markw/fpga/svn/repo/trunk/atari_800xl/packaged/";
 mkdir "$DEST";
 my $date = strftime("%Y%m%d",gmtime);
 my $dir = "$DEST/$date";
