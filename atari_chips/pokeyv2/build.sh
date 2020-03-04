0b5dd3675610da6e23b8cf585d419891e98b9c6c
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2020-03-04 21:48:59 +0000

    Added usual build script setup. Included 3 variants for 3 max 10 sizes.
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@987 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

---
+++ atari_chips/pokeyv2/build.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $wanted_variant = shift @ARGV;
+
+my $name="eclaireXL";
+
+#Added like this to the generated qsf
+#set_parameter -name TV 1
+
+my %variants = 
+(
+	"10M02_mono" =>
+	{
+		"stereo" => 0,
+		"fpga" => "10M02SCU169C8G"
+	},
+	"10M02_stereo_auto" =>
+	{
+		"stereo" => 1,
+		"enable_auto_stereo" => 1,
+		"fpga" => "10M02SCU169C8G"
+	},
+	"10M02_stereo_u1mb" =>
+	{
+		"stereo" => 1,
+		"enable_auto_stereo" => 0,
+		"fpga" => "10M02SCU169C8G"
+	},
+	"10M04_mono" =>
+	{
+		"stereo" => 0,
+		"fpga" => "10M04SCU169C8G"
+	},
+	"10M04_stereo_auto" =>
+	{
+		"stereo" => 1,
+		"enable_auto_stereo" => 1,
+		"fpga" => "10M04SCU169C8G"
+	},
+	"10M04_stereo_u1mb" =>
+	{
+		"stereo" => 1,
+		"enable_auto_stereo" => 0,
+		"fpga" => "10M04SCU169C8G"
+	},
+	"10M08_mono" =>
+	{
+		"stereo" => 0,
+		"fpga" => "10M08SCU169C8G"
+	},
+	"10M08_stereo_auto" =>
+	{
+		"stereo" => 1,
+		"enable_auto_stereo" => 1,
+		"fpga" => "10M08SCU169C8G"
+	},
+	"10M08_stereo_u1mb" =>
+	{
+		"stereo" => 1,
+		"enable_auto_stereo" => 0,
+		"fpga" => "10M08SCU169C8G"
+	}
+);
+
+if (not defined $wanted_variant or (not exists $variants{$wanted_variant} and $wanted_variant ne "ALL"))
+{
+	die "Provide variant of ALL or ".join ",",sort keys %variants;
+}
+
+foreach my $variant (sort keys %variants)
+{
+	next if ($wanted_variant ne $variant and $wanted_variant ne "ALL");
+	print "Building $variant of $name\n";
+
+	my $dir = "build_$variant";
+	`rm -rf $dir`;
+	mkdir $dir;
+	`cp *.vhd* $dir`;
+	`cp pokeymax*.sdc $dir`;
+	`cp pokeymax*.qpf $dir`;
+	`cp pokeymax*.qsf $dir`;
+	`cp -r int_osc* $dir`;
+	`cp -r pll* $dir`;
+
+	chdir $dir;
+
+	my $fpga = $variants{$variant}->{"fpga"};
+	
+	`echo set_global_assignment -name DEVICE $fpga >> pokeymax.qsf`;
+
+	foreach my $key (sort keys %{$variants{$variant}})
+	{
+		my $val = $variants{$variant}->{$key};
+		`echo set_parameter -name $key $val >> pokeymax.qsf`;
+	}
+
+	`quartus_sh --flow compile pokeymax > build.log 2> build.err`;
+
+	chdir "..";
+}
+
