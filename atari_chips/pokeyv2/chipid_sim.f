7516af0cb43cb2bd4c062b22f8cfda36256be62d
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2020-03-18 20:55:48 +0000

    Expose the MAX10 unique chip id
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@994 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

---
+++ atari_chips/pokeyv2/chipid_sim.f
@@ -0,0 +1,5 @@
+chipid_sim/chipid.vhd
+chipid_sim/altchip_id/mentor/altchip_id.v
+chipid_sim/altchip_id/cadence/altchip_id.v
+chipid_sim/altchip_id/synopsys/altchip_id.v
+chipid_sim/altchip_id/aldec/altchip_id.v
