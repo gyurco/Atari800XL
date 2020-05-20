d25f54cea53c7af6b29207fd603b8c7c23f1381a
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2020-05-20 13:14:00 +0000

    Some overflow issues
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@1080 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- atari_chips/pokeyv2/sigmadelta.vhd
+++ atari_chips/pokeyv2/sigmadelta.vhd
@@ -6,6 +6,8 @@
 -- This applies for source and binary form and derived works.
 ---------------------------------------------------------------------------
 --
+-- Simple sigma delta based on https://aip.scitation.org/doi/pdf/10.1063/1.3526240
+--
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -23,10 +25,10 @@ PORT
 END sigmadelta;
 
 ARCHITECTURE vhdl OF sigmadelta IS	
-	signal ttl1_next : signed(19 downto 0);
-	signal ttl1_reg : signed(19 downto 0);
-	signal ttl2_next : signed(19 downto 0);		
-	signal ttl2_reg : signed(19 downto 0);		
+	signal ttl1_next : signed(21 downto 0);
+	signal ttl1_reg : signed(21 downto 0);
+	signal ttl2_next : signed(21 downto 0);		
+	signal ttl2_reg : signed(21 downto 0);		
 	
 	signal out_next : std_logic;
 	signal out_reg : std_logic;
@@ -47,13 +49,13 @@ BEGIN
 
 
 	process(audin,ttl1_reg,ttl2_reg)
-		variable fb : signed(19 downto 0);
-		variable ttl1_tmp : signed(19 downto 0);
+		variable fb : signed(21 downto 0);
+		variable ttl1_tmp : signed(21 downto 0);
 	begin
-		if (ttl2_reg(19) = '1') then
-			fb := to_signed(0,20);
+		if (ttl2_reg(21) = '1') then
+			fb := to_signed(0,22);
 		else			
-			fb := to_signed(65536,20);
+			fb := to_signed(65536,22);
 		end if;
 	
 		ttl1_tmp := ttl1_reg + signed("0000"&audin) - fb;
@@ -61,7 +63,7 @@ BEGIN
 
 		ttl2_next <= ttl2_reg + ttl1_next - fb;	
 		
-		out_next <= not(ttl2_reg(19));
+		out_next <= not(ttl2_reg(21));
 	end process;
 
 	audout <= out_reg;
