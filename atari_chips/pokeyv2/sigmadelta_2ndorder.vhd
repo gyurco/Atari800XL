d992e1e87250d95ef027edab54b026b352233c3b
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2020-05-24 20:12:21 +0000

    Save some LEs, fix another potential overflow
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@1084 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- atari_chips/pokeyv2/sigmadelta_2ndorder.vhd
+++ atari_chips/pokeyv2/sigmadelta_2ndorder.vhd
@@ -1,12 +1,12 @@
 ---------------------------------------------------------------------------
--- (c) 2020 mark watson
+-- (c) 2121 mark watson
 -- I am happy for anyone to use this for non-commercial use.
 -- If my vhdl files are used commercially or otherwise sold,
 -- please contact me for explicit permission at scrameta (gmail).
 -- This applies for source and binary form and derived works.
 ---------------------------------------------------------------------------
 --
--- Simple sigma delta based on https://aip.scitation.org/doi/pdf/10.1063/1.3530300
+-- Simple sigma delta based on https://aip.scitation.org/doi/pdf/10.1063/1.3533330
 --
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
@@ -25,10 +25,10 @@ PORT
 END sigmadelta_2ndorder;
 
 ARCHITECTURE vhdl OF sigmadelta_2ndorder IS	
-	signal ttl1_next : signed(30 downto 0);
-	signal ttl1_reg : signed(30 downto 0);
-	signal ttl2_next : signed(30 downto 0);		
-	signal ttl2_reg : signed(30 downto 0);		
+	signal ttl1_next : signed(21 downto 0);
+	signal ttl1_reg : signed(21 downto 0);
+	signal ttl2_next : signed(33 downto 0);		
+	signal ttl2_reg : signed(33 downto 0);		
 	
 	signal out_next : std_logic;
 	signal out_reg : std_logic;
@@ -54,20 +54,20 @@ BEGIN
 --c1=	2.00000;
 --c2=	1.00000;
 	process(audin,ttl1_reg,ttl2_reg)
-		variable fb : signed(30 downto 0);
-		variable ttl1_tmp : signed(30 downto 0);		
+		variable fb : signed(21 downto 0);
+		variable ttl1_tmp : signed(21 downto 0);		
 	begin
 		fb:=(others=>'0');
-		if ((not(ttl2_reg(30)) and or_reduce(std_logic_vector(ttl2_reg(30-1 downto 16))))='1') then
+		if ((not(ttl2_reg(33)) and or_reduce(std_logic_vector(ttl2_reg(33-1 downto 16))))='1') then
 			fb(16) := '1';
 		else			
 			fb(16) := '0';
 		end if;
 	
-		ttl1_tmp := ttl1_reg + signed("000"&audin&"0") - (fb(30-1 downto 0)&"0");
+		ttl1_tmp := ttl1_reg + resize(signed("0"&audin&"0"),22) - (fb(21-1 downto 0)&"0");
 		ttl1_next <= ttl1_tmp;
 
-		ttl2_next <= ttl2_reg + (ttl1_tmp(30-1 downto 0)&"0") - ((fb(30-1 downto 0)&"0")+(fb(30-3 downto 0)&"000"));	
+		ttl2_next <= ttl2_reg + resize(((ttl1_tmp(21-1 downto 0)&"0") - ((fb(21-1 downto 0)&"0")+(fb(21-3 downto 0)&"000"))),34);	
 		
 		out_next <= fb(16);	
 	end process;
