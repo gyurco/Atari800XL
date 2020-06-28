5e5d64e77b4433c24ffe6f9fe442ac8294542340
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2020-06-28 18:22:01 +0000

    Cut sid size slightly by sharing pre-filter adder
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@1147 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- atari_chips/pokeyv2/SID/preFilterSum.vhdl
+++ atari_chips/pokeyv2/SID/preFilterSum.vhdl
@@ -32,6 +32,13 @@ ARCHITECTURE vhdl OF SID_preFilterSum IS
 	signal prefilter_next: signed(15 downto 0);
 	signal direct_reg: signed(15 downto 0);
 	signal direct_next: signed(15 downto 0);
+	signal acc_reg: signed(17 downto 0);
+	signal acc_next: signed(17 downto 0);	
+	signal phase_reg : unsigned(2 downto 0);
+	signal phase_next : unsigned(2 downto 0);
+	
+	signal channel_mux : signed(15 downto 0);
+	signal channel_sel : std_logic_vector(1 downto 0);
 BEGIN
 	-- register
 	process(clk, reset_n)
@@ -39,43 +46,76 @@ BEGIN
 		if (reset_n = '0') then
 			prefilter_reg <= (others=>'0');
 			direct_reg <= (others=>'0');
+			acc_reg <= (others=>'0');
+			phase_reg <= (others=>'0');
 		elsif (clk'event and clk='1') then
 			prefilter_reg <= prefilter_next;
 			direct_reg <= direct_next;
+			acc_reg <= acc_next;
+			phase_reg <= phase_next;
 		end if;
 	end process;
 	
 	-- next state
-	process(prefilter_reg,direct_reg,enable,channel_a,channel_b,channel_c,channel_c_cutdirect,filter_en)
-		variable sum_tmp : signed(17 downto 0);
-
-		variable filter_en0_ext : signed(15 downto 0);
-		variable filter_en1_ext : signed(15 downto 0);
-		variable filter_en2_ext : signed(15 downto 0);
-		variable filter_en2cd_ext : signed(15 downto 0);
+	process(phase_reg,acc_reg,prefilter_reg,direct_reg,enable,channel_c_cutdirect,filter_en,channel_mux)
+		variable filter_en0_ext : std_logic_vector(1 downto 0);
+		variable filter_en1_ext : std_logic_vector(1 downto 0);
+		variable filter_en2_ext : std_logic_vector(1 downto 0);
+		variable filter_en2cd_ext : std_logic_vector(1 downto 0);
+		
+		variable adder_result : signed(17 downto 0);
 	begin
 		prefilter_next <= prefilter_reg;
 		direct_next <= direct_reg;
-
+		acc_next <= acc_reg;
+		phase_next <= phase_reg;
+				
 		filter_en0_ext := (others=>filter_en(0));
 		filter_en1_ext := (others=>filter_en(1));
 		filter_en2_ext := (others=>filter_en(2));
-		filter_en2cd_ext := (others=>filter_en(2) or channel_c_cutdirect);
+		filter_en2cd_ext := (others=>filter_en(2) or channel_c_cutdirect);		
 		
-		if (enable = '1') then
-			sum_tmp := 
-				   resize(signed(channel_a and filter_en0_ext),18) + 
-				   resize(signed(channel_b and filter_en1_ext),18) + 
-				   resize(signed(channel_c and filter_en2_ext),18);
-			prefilter_next <= signed(sum_tmp(17 downto 2));
-
-			sum_tmp := 
-				   resize(signed(channel_a and not(filter_en0_ext)),18) + 
-				   resize(signed(channel_b and not(filter_en1_ext)),18) +
-				   resize(signed(channel_c and not(filter_en2cd_ext)),18);
-			direct_next <= signed(sum_tmp(17 downto 2));
-		end if;
+		phase_next <= phase_reg+1;
+		
+		adder_result := acc_reg + channel_mux;	
+	   acc_next <= adder_result;
+		
+		case phase_reg is
+		when "000" =>
+			channel_sel <= "01" and filter_en0_ext;
+		when "001" =>
+			channel_sel <= "10" and filter_en1_ext;
+		when "010" =>
+			channel_sel <= "11" and filter_en2_ext;		
+			prefilter_next	<= adder_result(17 downto 2);
+			acc_next <= (others=>'0');
+		when "011" =>
+			channel_sel <= "01" and not(filter_en0_ext);
+		when "100" =>
+			channel_sel <= "10" and not(filter_en1_ext);
+		when "101" =>
+			channel_sel <= "11" and not(filter_en2cd_ext);
+			phase_next <= (others=>'0');
+			direct_next <= adder_result(17 downto 2);
+			acc_next <= (others=>'0');
+		when others =>
+		end case;		
+		
 	end process;	
+	
+	process(channel_sel,channel_a,channel_b,channel_c)
+	begin
+		channel_mux <= (others=>'0');
+		case channel_sel is
+		when "01" =>
+			channel_mux <= channel_a;
+		when "10" =>
+			channel_mux <= channel_b;
+		when "11" =>
+			channel_mux <= channel_c;
+		when others =>
+		end case;
+	end process;
 		
 	-- output
 	prefilter_out <= prefilter_reg;
