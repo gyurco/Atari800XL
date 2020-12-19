3f1d00246c4794a8c155422210fb145901f3e073
markw <markw@d6161ac3-aae1-4d52-9b05-ea9ac36b53d3>
2020-12-19 19:07:42 +0000

    slope.m used to generate the data for the 6581 approx, with more details to be found in approx.m
    git-svn-id: http://www.64kib.com/atarixlfpga_svn/trunk/atari_800xl@1263 d6161ac3-aae1-4d52-9b05-ea9ac36b53d3

--- atari_chips/pokeyv2/SID/investigation/approx.m
+++ atari_chips/pokeyv2/SID/investigation/approx.m
@@ -23,8 +23,8 @@ func = @(x) scalefunc(func(x));
 test = csvread('/home/markw/fpga/svn/repo/trunk/atari_800xl/atari_chips/pokeyv2/SID/investigation/output_6.csv');
 s1 = test(test(:,1)==1,:);
 
-locoffset = -9.74126000000000
-locscale =  0.00664125000000000
+locoffset = -9.74126000000000;
+locscale =  0.00664125000000000;
 %besterr =    2.79586225092504e-08
 
 loc = locoffset+(s1(:,5)*locscale);
@@ -39,9 +39,10 @@ vals2 = nan(512,1024);
 
 stepsize = (16/1024);
 funclookup = func(-8:stepsize:(8-stepsize));
+%numel(unique(funclookup))
 
 delta =  0.0652017000000000; %median - real sigmoid
-shiftlookup = nan(1,512);
+%shiftlookup = nan(1,512);
 
 %each step is TYPE3_W0S_RESOLUTION
 %which is kind of annoying, lets make it 1/4096 instead 
@@ -51,25 +52,65 @@ shiftlookup = nan(1,512);
 FAST_W0S_RESOLUTION = 1/4096;
 adjres = (TYPE3_W0S_RESOLUTION/FAST_W0S_RESOLUTION);
 
-for i=1:512
-  shiftlookup(i) = adjres*delta*(i-1);
-end
+%for i=1:512
+%  shiftlookup(i) = adjres*delta*(i-1);
+%end
 
 locstep = loc*(1/stepsize);
-shiftlookup = shiftlookup*(1/stepsize);
-shiftlookup = shiftlookup + -1 +numel(funclookup)/2;
+%shiftlookup = shiftlookup*(1/stepsize);
+%shiftlookup = shiftlookup + -1 +numel(funclookup)/2;
+%debug> polyfit(1:numel(shiftlookup),shiftlookup,1)
+%ans =
+%
+%     3.4184   507.5816
 
+locstepc = locstep+507.5816;
+
+funclookup2 = interp1(1:numel(funclookup),funclookup,1:(3.4184):numel(funclookup));
+%N = numel(-8:stepsize:(8-stepsize));
+%funclookup2 = func(1:(3.4184):N);
+
+
+figure;
+%t = 1:(2^18/65):(2^18-1);
+%sigmoidreal = @(x) 1./(1.+e.^-x);
+%t = sigmoidreal(-4+8*t/2^18);
+%t=funclookup2(1) + t*(funclookup2(300)-funclookup2(1))
+
+locstepc = 3.4184+(locstepc/3.4184);
 for i=1:256
-  shift = shiftlookup(i);                  %lookup shift   (when vol changed enough)
-                                           %lookup loc     (when reg changed)
-  idx = locstep + shift;                   %adder
-  idx = min(max(1,idx),numel(funclookup)); %cap
-  vals2(i,:) = funclookup(round(idx));     %lookup sigmoid (when sum changed)
+  %shift = shiftlookup(i);                  %lookup shift   (when vol changed enough) - straigh line...
+  %shift  = 3.4184*i+507.5816;
+  %shiftnc  = 3.4184*i;
+  shiftnc  = i-1;
+                                           %lookup loc     (when f reg changed)
+  %idx = locstep + shift;                   %adder
+  idx = locstepc + shiftnc;                   %adder
+  idx = min(max(1,idx),numel(funclookup2)); %cap
+  vals2(i,:) = funclookup2(round(idx));     %lookup sigmoid (when sum changed)
+  %vals2(i,:) = func(round(idx));     %lookup sigmoid (when sum changed)
   
   %vals2(i,:) = func(loc.+(delta*(i-1)));
   
   %should work, but ... fast enough? 10 + 10 + 10 -> 30 cycles -> say 1.8MHz to update f? Probably ok...
 end
+
+%For vhdl... also in slope.m
+figure
+sigmoidvhdl=funclookup2(round(1:299/64:300));
+plot(sigmoidvhdl)
+
+rescale =  0.133923834850281;
+reoffset = 0.00188312488556502;
+sv2 = sigmoidreal(-8:16/64:8)*rescale + reoffset;
+hold all
+plot(sv2);
+
+for i=1:numel(sv2)
+  fprintf('"%s",',dec2bin(floor((2^18-1)*sv2(i)),18));
+endfor
+%For rom compute code
+
 
 %scale
 minf =  0.0018910;
